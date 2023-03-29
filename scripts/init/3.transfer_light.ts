2d8a71dc05fdbcb42a42291091b18d57c5e97b48
mixbee <mixbee.fan@gmail.com>
2023-03-29 22:28:43 +0800

    :hammer: update scripts

--- scripts/init/3.transfer_light.ts
+++ scripts/init/3.transfer_light.ts
@@ -5,14 +5,20 @@ import { FileUtils } from "../file_utils";
 async function main() {
 
   // todo
-  const newOwnerAddress = "0x00";
+  const teamVaultManager = "";
+  const teamVault = "";
+  const treasuryVault = "";
+  const foundationVault = "";
 
   const ltToken = await ethers.getContractAt("LT", FileUtils.getContractAddress(Constants.LT_TOKEN));
+  const smartWalletWhitelist = await ethers.getContractAt("SmartWalletWhitelist", FileUtils.getContractAddress(Constants.SmartWalletWhitelist));
+
+  await smartWalletWhitelist.approveWallet(teamVaultManager);
 
   console.log(await ltToken.totalSupply());
-  await ltToken.transfer(newOwnerAddress, ethers.utils.parseEther("300000000000"))
-  await ltToken.transfer(newOwnerAddress, ethers.utils.parseEther("50000000000"))
-  await ltToken.transfer(newOwnerAddress, ethers.utils.parseEther("50000000000"))
+  await ltToken.transfer(teamVault, ethers.utils.parseEther("300000000000"));
+  await ltToken.transfer(treasuryVault, ethers.utils.parseEther("50000000000"));
+  await ltToken.transfer(foundationVault, ethers.utils.parseEther("50000000000"));
 }
 
 main().catch((error) => {
